ab00d23c903f06b4d36d19c09e55de93252c2658
Olyno <alexlew95@gmail.com>
2019-09-04 18:08:40 +0200

    Fix code + added pipelines

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import DiscordAuth from './components/discord/DiscordAuth';
+import BitbucketAuth from './components/bitbucket/BitbucketAuth';
+import GithubAuth from './components/github/GithubAuth';
+
+export {
+    DiscordAuth,
+    BitbucketAuth,
+    GithubAuth
+}
+
+export default {
+    DiscordAuth,
+    BitbucketAuth,
+    GithubAuth
+}
